e6f6d9f4200ecb5a5d21c095e83f199f2349f5cf
linxford <linxford7@gmail.com>
2024-12-16 19:14:15 +0000

    slight mod

--- lib/src/widget/back_button.dart
+++ lib/src/widget/back_button.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_neumo/flutter_neumo.dart';
 
 class NeumoBackButton extends StatelessWidget {
